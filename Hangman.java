2a5f1acd477e31010e91d2cf8ae9da13e3866c42
MelathStofi <melath.stofi@gmail.com>
2019-09-10 13:36:29 +0200

    add simple array of words at first

--- Hangman.java
+++ Hangman.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Hangman {
+
+	private static String[] words = {"caterpillar", "human", "toxin", "crane", "pastel", "queue"};
+	
 	public static void main(String[] args) {
-		System.out.println("Így lex egy fájlba és kéx!");
+		System.out.println(Arrays.toString(words));
 	}
 }
