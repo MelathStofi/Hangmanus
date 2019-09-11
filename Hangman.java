9e31cbc3f1edb048d8c7f545e7a66e79b5c16297
MelathStofi <melath.stofi@gmail.com>
2019-09-11 10:33:15 +0200

    refactor

--- Hangman.java
+++ Hangman.java
@@ -5,38 +5,44 @@ public class Hangman {
 
 	private static String[] words = {"caterpillar", "human", "toxin", "crane", "pastel", "queue"};
 	
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(words));
-
-		String[] fruits = { "orange", "apple", "strawberry", "banana", "pear" };
-        int arrayLength = fruits.length;
+	private static String getRandomWord() {
+		int arrayLength = words.length;
         int randomChoice = (int)(Math.random()*arrayLength);
-        String fruit = fruits[randomChoice];
-        System.out.println(fruit); // shows the fruit for easier testing
+        String word = words[randomChoice];
+        return word;
+	}
+	
+	private static char getCharInput() {
+		// Getting a char input (guessing letter) from the user
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a letter:");
+        char letter = input.next().charAt(0);
+        return letter;
+	}
+	
+	public static void main(String[] args) {
+	
+		System.out.println(Arrays.toString(words));
+		String word = getRandomWord();
+        System.out.println(word); // shows the word for easier testing
         
         boolean win = false;
         while( win != true ){
         
-            // Getting a char input (guessing letter) from the user
-            Scanner letter = new Scanner(System.in);
-            System.out.println("Guess a letter:");
-            char guessedLetter = letter.next().charAt(0); 
+            char guessedLetter = getCharInput();
 
+            // Iterating trough the random chose word letters
+            for(int i = 0; i < word.length(); i++){
+                char wordChar = word.charAt(i);
 
-            // Iterating trough the random choosed fruit letters
-            for(int i = 0; i < fruit.length(); i++){
-                char fruitChar = fruit.charAt(i);
-
-                if(fruitChar == guessedLetter){
-                    System.out.print(fruitChar);
+                if(wordChar == guessedLetter){
+                    System.out.print(wordChar);
                 }
                 else{
-                    System.out.print("-");
+                    System.out.print("_");
                 }
             }
             System.out.println("");
-
-           // System.out.println(guessedFruitLetters);
         };
 	}
 }
