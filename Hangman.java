4a022b7e153352cee5024f564091f84546355c5a
Geriiick <zeleig96@gmail.com>
2019-09-11 09:49:54 +0200

    Added random choice, and user input

--- Hangman.java
+++ Hangman.java
@@ -6,5 +6,38 @@ public class Hangman {
 	
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(words));
+
+		String[] fruits = { "orange", "apple", "strawberry", "banana", "pear" };
+        int arrayLength = fruits.length;
+        int randomChoice = (int)(Math.random()*arrayLength);
+        String fruit = fruits[randomChoice];
+        System.out.println(fruit); // shows the fruit for easier testing
+        
+        boolean win = false;
+        while( win = true ){
+
+
+
+            // Getting a char input (guessing letter) from the user
+            Scanner letter = new Scanner(System.in);
+            System.out.println("Guess a letter:");
+            char guessedLetter = letter.next().charAt(0); 
+
+
+            // Iterating trough the random choosed fruit letters
+            for(int i = 0; i < fruit.length(); i++){
+                char fruitChar = fruit.charAt(i);
+
+                if(fruitChar == guessedLetter){
+                    System.out.print(fruitChar);
+                }
+                else{
+                    System.out.print("-");
+                }
+            }
+            System.out.println('_');
+
+           // System.out.println(guessedFruitLetters);
+        };
 	}
 }
