f8feea64249d741a04692f38745e7b48032f9643
MelathStofi <melath.stofi@gmail.com>
2019-09-11 13:42:14 +0200

    add win-check

--- Hangman.java
+++ Hangman.java
@@ -33,7 +33,7 @@ public class Hangman {
 		}
 	}
 	
-	private static void checkLetter(char[] result, char guessedLetter, String word) {
+	private static void insertLetter(char[] result, char guessedLetter, String word) {
 		// Iterating trough the letters of the randomly chosen word
         for(int i = 0; i < word.length(); i++){
             char wordChar = word.charAt(i);
@@ -43,24 +43,33 @@ public class Hangman {
         }
 	}
 	
+	private static boolean checkWin(char[] result, int length) {
+		for (int i = 0; i < length; i++) {
+			if (result[i] == '_') {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		boolean playTime = true;
-		while (playTime == true) { 
-	
+		while (playTime == true) {
 			System.out.println(Arrays.toString(words));
 			String word = getRandomWord();
 			char result[] = new char[word.length()];
 			fillResultArray(result, word.length());
 		    System.out.println(word); // shows the word for easier testing
 		    
-		    boolean win = false;
-		    while( win != true ){
-		    
+		    boolean isWin = false;
+		    while( isWin != true ){
 		        char guessedLetter = getCharInput();
-				checkLetter(result, guessedLetter, word);
+				insertLetter(result, guessedLetter, word);
 		        System.out.println(result);
 		        System.out.println("");
+				isWin = checkWin(result, word.length());
 		    }
+		    System.out.println("Victory");
         }
 	}
 }
