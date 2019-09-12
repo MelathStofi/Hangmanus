9f899cf47a5b87768e22e32652cfd78f80558a95
MelathStofi <melath.stofi@gmail.com>
2019-09-12 11:16:41 +0200

    add play again option

--- Hangman.java
+++ Hangman.java
@@ -20,9 +20,8 @@ public class Hangman {
 	}
 	
 	private static char getCharInput() {
-	// Getting a char input (guessing letter) from the user
+	// Getting a char input from the user
         Scanner input = new Scanner(System.in);
-        System.out.println("Guess a letter:");
         char letter = input.next().charAt(0);
         return letter;
 	}
@@ -33,14 +32,17 @@ public class Hangman {
 		}
 	}
 	
-	private static void insertLetter(char[] result, char guessedLetter, String word) {
+	private static boolean insertLetter(char[] result, char guessedLetter, String word) {
 		// Iterating trough the letters of the randomly chosen word
+		boolean isInWord = false;
         for(int i = 0; i < word.length(); i++){
             char wordChar = word.charAt(i);
             if(wordChar == guessedLetter){
                 result[i] = wordChar;
+                isInWord = true;
             }
         }
+        return isInWord;
 	}
 	
 	private static boolean checkWin(char[] result, int length) {
@@ -57,19 +59,43 @@ public class Hangman {
 		while (playTime == true) {
 			System.out.println(Arrays.toString(words));
 			String word = getRandomWord();
+			int wrongGuessNumber = 0;
+			char[] wrongGuesses = new char[6];
 			char result[] = new char[word.length()];
 			fillResultArray(result, word.length());
 		    System.out.println(word); // shows the word for easier testing
 		    
 		    boolean isWin = false;
 		    while( isWin != true ){
+		    	System.out.println("\nGuess a letter:");
 		        char guessedLetter = getCharInput();
-				insertLetter(result, guessedLetter, word);
+				boolean isRightGuess = insertLetter(result, guessedLetter, word);
+				if (isRightGuess == false) {
+					wrongGuesses[wrongGuessNumber] = guessedLetter;
+					wrongGuessNumber++;
+				}
+				if (wrongGuessNumber != 0) {
+					System.out.println("Wrong letters: ");
+					for (int i = 0; i < wrongGuessNumber; i++) {
+			   			System.out.print(wrongGuesses[i] + "  ");
+			   		}
+			   		System.out.println("");
+		   		}
+		   		System.out.println("");
 		        System.out.println(result);
 		        System.out.println("");
 				isWin = checkWin(result, word.length());
 		    }
-		    System.out.println("Victory");
+		    System.out.println("\nVictory!\n");
+		    System.out.println("Another game? (y/n): ");
+		    char playAgain = ' ';
+		    while (playAgain != 'y' && playAgain != 'Y') {
+				playAgain = getCharInput();
+				if (playAgain == 'n' || playAgain == 'N') {
+					playTime = false;
+					break;
+				}
+		    }
         }
 	}
 }
